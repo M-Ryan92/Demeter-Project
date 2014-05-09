d118e598a69619e0fa68b8b620a39eed8652f3d6
michel.dolstra <mdo@woodwing.com>
2014-05-09 02:03:31 +0200

    issue #16 : add build script + grunt (Todo optimize this)

---
+++ WebSublimateCMS/Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+
+var Globals = grunt.file.readYAML('../globals.yaml');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                maxerr: 25,
+                eqeqeq: true,
+                regexdash: true,
+                scripturl: true,
+                forin: true,
+                unused: true,
+                devel: true,
+                undef: true,
+                browser: true,
+                dojo: true
+            },
+            webSublimateCMS: {
+                src: Globals.js
+            }    },
+        sass: {
+            webSublimateCMS: {
+                options: {
+                    compass: true,
+                    style: "compressed"
+                },
+                files: [
+                    {
+                        src: "src/view/main.scss",
+                        dest: Globals.dist.css + "/webSublimateCMS.css"
+                    }
+                ]
+            }    },
+        copy: {
+            webSublimateCMS: {
+                files: [
+                    {
+                        expand: true,
+                        src: Globals.html,
+                        dest: '.grunt/grunt-contrib-jasmine'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: Globals.images,
+                        dest: Globals.dist.img
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'sass', 'copy']);
+    grunt.registerTask('validate', ['jshint']);
+    grunt.registerTask('optimise', ['sass', 'copy']);
+};
