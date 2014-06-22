e56dc2af13c282bce1b47df827a9d8c11d3d1001
Teije van Sloten <teije.van.sloten@hva.nl>
2014-06-22 17:52:44 +0200

    Second include for the header.

---
+++ Webclient/lib/basicelements/header.php
@@ -0,0 +1,52 @@
+<div class="main container">
+            <div class="navbar navbar-default" role="navigation">
+                <div class="row textcenter">
+                    <div class="navbar-right navbar-collapse collapse">
+                        <div class="input-group">
+                            <input type="text" placeholder="Search" class="form-control">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                            </span>
+                        </div>
+                    </div>
+                    <img class="navbar-brand" src="img/demeter.png" alt="">
+                    <span class="title">Demeter voeding en welzijn</span>
+                    <button type="button" class="navbar-toggle navbar-right"  data-toggle="collapse" data-target=".dropdownmenu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class=""><a href="#">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            <li><a href="#cat1">Cat1</a></li>
+                            <li><a href="#cat2">Cat2</a></li>
+                            <li><a href="#cat3">Cat3</a></li>
+                            <li><a href="#cat4">Cat4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <div class="nav-lable-style">
+                    <div>
+                        Home and make this lable extend a bit
+                    </div>
+                </div>
+                <div class="dropdownmenu collapse submenu">
+                    <ul class="nav navbar-nav">
+                        <li class="empty"></li>
+                        <li class=""><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#cat1">Cat1</a></li>
+                        <li><a href="#cat2">Cat2</a></li>
+                        <li><a href="#cat3">Cat3</a></li>
+                        <li><a href="#cat4">Cat4</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
