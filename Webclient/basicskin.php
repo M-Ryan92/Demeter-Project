eb64ab99da2c1252cf16bfbe7737c986ec3ff0b7
Teije van Sloten <teije.van.sloten@hva.nl>
2014-06-22 17:00:53 +0200

    My shit, will merge soon with header.

--- Webclient/basicskin.php
+++ Webclient/basicskin.php
@@ -5,29 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="lib/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="../resources/Bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="lib/css/basicstyle.css" rel="stylesheet" type="text/css">
-        <script type="text/javascript" src="js/jquery-2.1.0.min.js" ></script>
-        <script type="text/javascript" src="js/bootstrap.js"></script>
     </head>
     <body>
-        <div class="container">
-            <nav class="navbar navbar-default" role="navigation">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Brand</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
-
         <div class="container">
             <!-- img slider -->
             <div class="row">
@@ -43,30 +24,34 @@
             </div>
 
             <!-- subscribe button -->
-            <div class="row">
-                <button class="btn btn-default" style="display: block;margin: 10px auto 10px auto;">
-                    Inschrijven voor de nieuwsbrief
-                </button>
+            <div class="row" style="margin: 15px 0 15px 0;height: 34px;">
+                <div style="margin: 0px auto;width: 300px;">
+                    
+                    <button class="btn btn-default" style="width:300px;" onclick="setVisible(this)">Inschrijven voor de Nieuwsbrief!</button>
+                    <input type="email" style="width:200px; float: left; border-radius: 4px 0px 0px 4px; border-right: none; display: none;" id="newsinput" class="form-control" />
+                    <button class="btn btn-success" style="width:100px; border-radius: 0 4px 4px 0; display: none;" id="newsbutton">Inschrijven</button>
+                </div>
+                
             </div>
 
             <!-- Voorstellen pagina -->
             <div class="row">
                 <div class="col-md-offset-1 col-md-10" style="height: 400px">
                     <div class="col-md-6 text-center">
-                        <img src="/img/demeter.png" alt="" style="width:140px; height: 140px;">
+                        <img src="img/demeter.png" alt="" style="width:140px; height: 140px;">
                         <h2>Demeter, voeding en welzijn</h2>
                         <p>Om naar een zo goed mogelijke gezondheid te streven is het van belang dat lichaam
                             en geest in balans zijn. Voeding, stressbeperking en beweging zijn drie belangrijke factoren voor een optimaal evenwicht...</p>
                         <p><a class="btn btn-success" href="#" role="button">Lees meer &raquo;</a></p>
                     </div>
                     <div class="col-md-6 text-center">
-                        <img src="/img/karin.jpg" alt="" class="img-circle" style="width:140px; height: 140px;">
+                        <img src="img/karin.jpg" alt="" class="img-circle" style="width:140px; height: 140px;">
                         <h2>Even voorstellen</h2>
                         <p>Mijn naam is Karin Papadopoulos-Broers, geboren in 1965, getrouwd, trotse moeder van drie zonen,
                             met een passie voor gezonde voeding, koken en sport. Op een bepaald moment in mijn leven, was ik de balans behoorlijk kwijt...</p>
                         <p><a class="btn btn-success" href="#" role="button">Lees meer &raquo;</a></p>
                     </div>
-                    
+
                 </div>
             </div>
 
@@ -77,29 +62,35 @@
 
     <div id="footer">
         <div class="container">
-            <div id="row">
-                <div class="col-md-3 col-md-offset-3 borderrightfooter">
-                    <ul class="col-md-12 list-unstyled">
+            <div>
+                <div class="col-md-4 col-md-offset-2">
+                    <h4>Algemeen</h4>
+                    <ul class="col-md-12">
                         <li><a href="#">Algemene voorwaarden</a></li>
                         <li><a href="#">Contact</a></li>
                         <li><a href="#">Tarieven</a></li>
                         <li><a href="#">Agenda</a></li>
                     </ul>	
                 </div>
-                <div class="col-md-4 footerleft">
-                    <div style="height: 70px;width: 70px;float:right;margin-top: 5px;">
-                        <img src="/img/demeter.png" alt="" style="height: 70px;width: 70px;">
+                <div class="col-md-4 col-md-offset-2 footerleft">
+                    <div style="height: 70px;width: 70px;float:right;margin-top: 25px;">
+                        <img src="img/demeter.png" alt="" style="height: 70px;width: 70px;">
                     </div>
-                    <h4 class="footertitle">Demeter - voeding en welzijn</h4>
+                    <h4>Demeter, voeding en welzijn</h4>
                     <ul class="list-unstyled">
                         <li>Langelende laan 4</li>
-                        <li>info@demetervw.nl</li>
+                        <li><a href="">info@demetervw.nl</a></li>
                         <li>tel. 0612345678</li>
                     </ul>
                 </div>
             </div>
-
+            <div class="col-md-12">
+                <p class="text-muted text-center" style="margin-top: 15px;">Powered by <a href="">Webcms</a></p>
+            </div>
         </div>
     </div>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../resources/Bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../lib/js/newssubscription.js"></script>
 </body>
 </html>
