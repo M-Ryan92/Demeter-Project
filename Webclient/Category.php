460f358ce2092cfa6da7619bab3226eb19dac87f
dolstrm001 <michel.dolstra@gmail.com>
2014-06-25 20:47:16 +0200

    add base html stuff with include and move to webclient

---
+++ Webclient/Category.php
@@ -0,0 +1,200 @@
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="../resources/Bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="webClient.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+		<?php include('baseComponents/header/header.php'); ?>
+
+		<div class="container" style="width:100% min-width:400px; padding: 30px 0;">
+			<!-- category header -->
+			<div class="container col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2" style="
+				background-color: rgba(31, 131, 197, 0.2);
+				height: 170px;
+				padding: 10px 10px;
+				border-radius: 25px;
+				-moz-border-radius: 25px;
+				-webkit-border-radius: 25px;
+				border-radius: 25px;
+				min-width:400px;
+				">
+				<div class="col-xs-7 col-sm-8 col-md-9" style="
+					height: 150px; overflow: hidden; text-overflow: ellipsis; padding: 5px;
+					">
+					<span>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus eros, posuere quis pharetra et, auctor in lacus. Donec elementum risus ut nunc dictum interdum. Vivamus rutrum dapibus diam sit amet facilisis. Curabitur ut est aliquet, interdum quam at, convallis ante. Cras non nunc et enim facilisis volutpat. Pellentesque eget felis vitae mauris placerat vulputate vitae eget turpis. Duis eleifend felis sit amet molestie fringilla. Quisque et gravida dolor. Aliquam quis arcu in orci luctus blandit. Proin non ligula vel ipsum blandit aliquam eget ac est. Phasellus vitae tortor non erat aliquam ornare vel pellentesque mauris. Fusce nec mauris urna. Duis tristique ultrices felis a sollicitudin. Pellentesque ullamcorper, ante vitae bibendum sodales, metus turpis volutpat metus, et accumsan eros mauris sed justo. Suspendisse id leo nec magna tempus sodales eu et sapien
+					</span>
+				</div>
+				<div class="col-xs-5 col-sm-4 col-md-3" style="text-align: end;">
+					<img src="http://www.doeners.nl/wp-content/uploads/2013/12/workshop.jpg" style="
+						height:148px; width:150px;
+						border-radius: 90px;
+						-moz-border-radius: 90px;
+						-webkit-border-radius: 90px;
+						border-radius: 90px;				 
+						"/>
+				</div>
+			</div>
+			<!-- list of workshops -->
+			<div class="container col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2" style="
+				min-width:400px;
+				background-color: rgba(31, 131, 197, 0.2);
+				height: 480px;
+				padding: 0;
+				margin-top: 35px;
+				border-radius: 25px;
+				-moz-border-radius: 25px;
+				-webkit-border-radius: 25px;
+				border-radius: 25px;">
+				<!-- list header -->
+				<div class="col-xs-12" style="
+				height: 50px; 
+				border-bottom: 3px solid white;
+				border-top-left-radius: 25px;
+				border-top-right-radius: 25px;
+				-moz-border-radius-topleft: 25px;
+				-moz-border-radius-topright: 25px;
+				-webkit-border-radius-topleft: 25px;
+				-webkit-border-radius-topright: 25px;
+				padding: 10px 15px;">
+					<div class="col-xs-5">
+						<div class="btn-group"> 
+							<button class="btn btn-sm btn-default" style="height: 28px;">ON</button>
+							<button class="btn btn-sm btn-primary active" style="height: 28px;">OFF</button>
+						</div>
+					</div>
+					<div style="text-align: end; z-index: 1;" class="col-xs-7">				
+						<div class="input-group" style="width:175px; display: inline-table; text-align:start;">
+							<input type="text" placeholder="Search workshop" class="form-control" style="height: 28px;">
+							<span class="input-group-btn">
+								<button class="btn btn-default" style="padding: 3px 10px;" type="button"><span class="glyphicon glyphicon-search"></span></button>
+							</span>
+						</div>
+					</div>			
+				</div>
+				<div class="col-xs-12" style="
+				height: 100%;
+				margin-top: -50px;
+				padding: 50px 0;
+				">
+					<!-- list item view -->
+					<div class="col-xs-12" style="
+					height:100%;
+					border-bottom: 3px solid white;
+					overflow-y:auto;
+					padding: 5px 20px;
+					">
+						
+						<!-- item 1 -->
+						<div class="container col-xs-12" style="
+							background-color: rgba(179, 110, 35, 0.2);
+							padding: 10px 10px;
+							margin: 5px 0
+							">
+							<div class="col-xs-7 col-sm-8 col-md-9" style="
+								height: 110px; overflow: hidden; text-overflow: ellipsis; padding: 5px;
+								">
+								<span>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus eros, posuere quis pharetra et, auctor in lacus. Donec elementum risus ut nunc dictum interdum. Vivamus rutrum dapibus diam sit amet facilisis. Curabitur ut est aliquet, interdum quam at, convallis ante. Cras non nunc et enim facilisis volutpat. Pellentesque eget felis vitae mauris placerat vulputate vitae eget turpis. Duis eleifend felis sit amet molestie fringilla. Quisque et gravida dolor. Aliquam quis arcu in orci luctus blandit. Proin non ligula vel ipsum blandit aliquam eget ac est. Phasellus vitae tortor non erat aliquam ornare vel pellentesque mauris. Fusce nec mauris urna. Duis tristique ultrices felis a sollicitudin. Pellentesque ullamcorper, ante vitae bibendum sodales, metus turpis volutpat metus, et accumsan eros mauris sed justo. Suspendisse id leo nec magna tempus sodales eu et sapien
+								</span>
+							</div>
+							<div class="col-xs-5 col-sm-4 col-md-3" style="text-align: end;">
+								<img src="http://www.doeners.nl/wp-content/uploads/2013/12/workshop.jpg" style="
+									height:108px; width:110px;
+									border-radius: 90px;
+									-moz-border-radius: 90px;
+									-webkit-border-radius: 90px;
+									border-radius: 90px;				 
+									"/>
+							</div>
+						</div>
+						<!-- item 2 -->
+						<div class="container col-xs-12" style="
+							background-color: rgba(179, 110, 35, 0.2);
+							padding: 10px 10px;
+							margin: 5px 0
+							">
+							<div class="col-xs-7 col-sm-8 col-md-9" style="
+								height: 110px; overflow: hidden; text-overflow: ellipsis; padding: 5px;
+								">
+								<span>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus eros, posuere quis pharetra et, auctor in lacus. Donec elementum risus ut nunc dictum interdum. Vivamus rutrum dapibus diam sit amet facilisis. Curabitur ut est aliquet, interdum quam at, convallis ante. Cras non nunc et enim facilisis volutpat. Pellentesque eget felis vitae mauris placerat vulputate vitae eget turpis. Duis eleifend felis sit amet molestie fringilla. Quisque et gravida dolor. Aliquam quis arcu in orci luctus blandit. Proin non ligula vel ipsum blandit aliquam eget ac est. Phasellus vitae tortor non erat aliquam ornare vel pellentesque mauris. Fusce nec mauris urna. Duis tristique ultrices felis a sollicitudin. Pellentesque ullamcorper, ante vitae bibendum sodales, metus turpis volutpat metus, et accumsan eros mauris sed justo. Suspendisse id leo nec magna tempus sodales eu et sapien
+								</span>
+							</div>
+							<div class="col-xs-5 col-sm-4 col-md-3" style="text-align: end;">
+								<img src="http://www.doeners.nl/wp-content/uploads/2013/12/workshop.jpg" style="
+									height:108px; width:110px;
+									border-radius: 90px;
+									-moz-border-radius: 90px;
+									-webkit-border-radius: 90px;
+									border-radius: 90px;				 
+									"/>
+							</div>
+						</div>				
+						<!-- item 3 -->
+						<div class="container col-xs-12" style="
+							background-color: rgba(179, 110, 35, 0.2);
+							padding: 10px 10px;
+							margin: 5px 0
+							">
+							<div class="col-xs-7 col-sm-8 col-md-9" style="
+								height: 110px; overflow: hidden; text-overflow: ellipsis; padding: 5px;
+								">
+								<span>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus eros, posuere quis pharetra et, auctor in lacus. Donec elementum risus ut nunc dictum interdum. Vivamus rutrum dapibus diam sit amet facilisis. Curabitur ut est aliquet, interdum quam at, convallis ante. Cras non nunc et enim facilisis volutpat. Pellentesque eget felis vitae mauris placerat vulputate vitae eget turpis. Duis eleifend felis sit amet molestie fringilla. Quisque et gravida dolor. Aliquam quis arcu in orci luctus blandit. Proin non ligula vel ipsum blandit aliquam eget ac est. Phasellus vitae tortor non erat aliquam ornare vel pellentesque mauris. Fusce nec mauris urna. Duis tristique ultrices felis a sollicitudin. Pellentesque ullamcorper, ante vitae bibendum sodales, metus turpis volutpat metus, et accumsan eros mauris sed justo. Suspendisse id leo nec magna tempus sodales eu et sapien
+								</span>
+							</div>
+							<div class="col-xs-5 col-sm-4 col-md-3" style="text-align: end;">
+								<img src="http://www.doeners.nl/wp-content/uploads/2013/12/workshop.jpg" style="
+									height:108px; width:110px;
+									border-radius: 90px;
+									-moz-border-radius: 90px;
+									-webkit-border-radius: 90px;
+									border-radius: 90px;				 
+									"/>
+							</div>
+						</div>
+						<!-- item 4 -->
+						<div class="container col-xs-12" style="
+							background-color: rgba(179, 110, 35, 0.2);
+							padding: 10px 10px;
+							margin: 5px 0
+							">
+							<div class="col-xs-7 col-sm-8 col-md-9" style="
+								height: 110px; overflow: hidden; text-overflow: ellipsis; padding: 5px;
+								">
+								<span>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus eros, posuere quis pharetra et, auctor in lacus. Donec elementum risus ut nunc dictum interdum. Vivamus rutrum dapibus diam sit amet facilisis. Curabitur ut est aliquet, interdum quam at, convallis ante. Cras non nunc et enim facilisis volutpat. Pellentesque eget felis vitae mauris placerat vulputate vitae eget turpis. Duis eleifend felis sit amet molestie fringilla. Quisque et gravida dolor. Aliquam quis arcu in orci luctus blandit. Proin non ligula vel ipsum blandit aliquam eget ac est. Phasellus vitae tortor non erat aliquam ornare vel pellentesque mauris. Fusce nec mauris urna. Duis tristique ultrices felis a sollicitudin. Pellentesque ullamcorper, ante vitae bibendum sodales, metus turpis volutpat metus, et accumsan eros mauris sed justo. Suspendisse id leo nec magna tempus sodales eu et sapien
+								</span>
+							</div>
+							<div class="col-xs-5 col-sm-4 col-md-3" style="text-align: end;">
+								<img src="http://www.doeners.nl/wp-content/uploads/2013/12/workshop.jpg" style="
+									height:108px; width:110px;
+									border-radius: 90px;
+									-moz-border-radius: 90px;
+									-webkit-border-radius: 90px;
+									border-radius: 90px;				 
+									"/>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php include('baseComponents/footer/footer.php'); ?>
+
+        <script type="text/javascript" src="../resources/JQuery/jquery.min.js"></script>
+        <script type="text/javascript" src="../resources/Bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
