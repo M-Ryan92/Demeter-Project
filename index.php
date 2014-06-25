89294e8cfa946dabc95ecd738c22807da0ee6104
dolstrm001 <michel.dolstra@gmail.com>
2014-06-25 16:48:36 +0200

    work in progress basic content view created category info field and part of workshop list added most of it is styling
    todo:
    fix list view and thumbnail view
    fix scss file

---
+++ index.php
@@ -0,0 +1,75 @@
+<link href="resources/Bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<div style="width:100% min-width:400px; padding: 30px 0;">
+	<!-- category header -->
+	<div class="container col-xs-12 col-sm-8 col-md-8 col-sm-offset-2 col-md-offset-2" style="
+		background-color: rgba(31, 131, 197, 0.2);
+		height: 170px;
+		padding: 10px 10px;
+		border-radius: 25px;
+		-moz-border-radius: 25px;
+		-webkit-border-radius: 25px;
+		border-radius: 25px;
+		min-width:400px;
+		">
+		<div class="col-xs-7 col-sm-8 col-md-9" style="
+			height: 150px; overflow: hidden; text-overflow: ellipsis; padding: 5px;
+			">
+			<span>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus eros, posuere quis pharetra et, auctor in lacus. Donec elementum risus ut nunc dictum interdum. Vivamus rutrum dapibus diam sit amet facilisis. Curabitur ut est aliquet, interdum quam at, convallis ante. Cras non nunc et enim facilisis volutpat. Pellentesque eget felis vitae mauris placerat vulputate vitae eget turpis. Duis eleifend felis sit amet molestie fringilla. Quisque et gravida dolor. Aliquam quis arcu in orci luctus blandit. Proin non ligula vel ipsum blandit aliquam eget ac est. Phasellus vitae tortor non erat aliquam ornare vel pellentesque mauris. Fusce nec mauris urna. Duis tristique ultrices felis a sollicitudin. Pellentesque ullamcorper, ante vitae bibendum sodales, metus turpis volutpat metus, et accumsan eros mauris sed justo. Suspendisse id leo nec magna tempus sodales eu et sapien
+			</span>
+		</div>
+		<div class="col-xs-5 col-sm-4 col-md-3" style="text-align: end;">
+			<img src="http://www.doeners.nl/wp-content/uploads/2013/12/workshop.jpg" style="
+				height:148px; width:150px;
+				border-radius: 90px;
+				-moz-border-radius: 90px;
+				-webkit-border-radius: 90px;
+				border-radius: 90px;				 
+				"/>
+		</div>
+	</div>
+	<!-- list of workshops -->
+	<div class="container col-xs-12 col-sm-8 col-md-8 col-sm-offset-2 col-md-offset-2" style="
+		min-width:400px;
+		background-color: rgba(31, 131, 197, 0.2);
+		height: 350px;
+		padding: 0;
+		margin-top: 35px;
+		border-radius: 25px;
+		-moz-border-radius: 25px;
+		-webkit-border-radius: 25px;
+		border-radius: 25px;
+	">
+		<div class="col-md-12" style="
+		height: 50px; 
+		border-bottom: 2px solid white;
+		border-top-left-radius: 25px;
+		border-top-right-radius: 25px;
+		-moz-border-radius-topleft: 25px;
+		-moz-border-radius-topright: 25px;
+		-webkit-border-radius-topleft: 25px;
+		-webkit-border-radius-topright: 25px;
+		padding: 10px 15px;
+		">
+			<div class="col-xs-5">
+				<div class="btn-group"> 
+					<button class="btn btn-sm btn-default" style="height: 28px;">ON</button>
+					<button class="btn btn-sm btn-primary active" style="height: 28px;">OFF</button>
+				</div>
+			</div>
+			<div style="text-align: end;" class="col-xs-7">
+				<div class="input-group" style="width:175px; display: inline-table; text-align:start;">
+					<input type="text" placeholder="Search workshop" class="form-control" style="height: 28px;">
+					<span class="input-group-btn">
+						<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+					</span>
+				</div>
+			</div>			
+		</div>
+
+	</div>
+
+</div>
+        <script type="text/javascript" src="resources/JQuery/jquery.min.js"></script>
+        <script type="text/javascript" src="resources/Bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="Webclient/js/newssubscription.js"></script>
