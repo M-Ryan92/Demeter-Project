f42666949fba6796723f5a6431b34573c70f73c3
michel.dolstra <mdo@woodwing.com>
2014-06-29 18:40:46 +0200

    add footer links and create base voorwaarden + tarieven pages

---
+++ Webclient/tarieven.php
@@ -0,0 +1,69 @@
+<?php include('globalsettings.php'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="<?php echo $bootstrapCSS; ?>" rel="stylesheet" type="text/css">
+		<link href="<?php echo $baseCSS; ?>" rel="stylesheet" type="text/css">
+	</head>
+	<body id='footermargin'>
+		<?php include($baseComponents."header/header.php"); ?>
+
+		<div class="container contentview">
+			<div class="voorwaarden">
+				<div class="col-md-12">
+					<h1 class="text-center" style="margin:0;">Tarieven</h1>
+				</div>
+
+				<div class="col-sm-offset-1 col-md-offset-2 col-xs-12 col-sm-10 col-md-8">
+					<h5>
+						Natuurvoedingsconsult en Kennismaking
+					</h5>
+					<span>
+						Voor een eerste kennismaking kun je mij altijd bellen of mailen. Wil je hierna graag verder dan mail ik je een vragenlijst en maken we een afspraak voor het intakegesprek.
+					</span>
+					<h5>
+						Intakegesprek
+					</h5>
+					<span>
+						Tijdens de intake bespreken we aan de hand van de ingevulde vragenlijst je doelen, huidige voedingspatroon, bewegingspatroon en eventuele lichamelijke klachten. Afhankelijk van je doel, zal ik je meten en wegen. Aan het eind van het gesprek bespreken we de mogelijkheden voor een vervolgtraject op basis van je wensen. Via de mail ontvang je tevens een schriftelijk advies, afgestemd op je leefpatroon en doelen.
+					</span>
+					<h6>
+						Duur: ca 60 minuten. Kosten: € 60
+					</h6>
+					<h5>
+						Vervolgconsulten
+					</h5>
+					<span>
+						Tijdens het vervolggesprek kijken we of je de gegeven adviezen hebt kunnen toepassen in je dagelijkse leven en waar nog eventuele knelpunten zijn. Is je doel afslanken dan meet en weeg ik je nogmaals  en bekijken we aan de hand van ontwikkelingen in spiermassa of je  op een gezonde wijze aan het afslanken bent. Afhankelijk van je wensen, krijg je aanvullend advies en informatie.
+					</span>
+					<h6>
+						Duur: ca 30 minuten. Kosten € 30 </br>
+						Pakket : Intakegesprek + 5 vervolgafspraken
+					</h6>
+					<span>
+						Als je graag wat intensievere begeleiding wil en een stok achter de deur is dit pakket een goede optie.  Na het intakegesprek zien we elkaar dan in een voor jou prettige frequentie, waarbij je iedere keer wordt gemeten en gewogen om de voortgang inzichtelijk te maken.
+					</span>
+					<h6>
+						Duur: intakegesprek ca. 60 minuten, de vervolgafspraken ca. 30 minuten. Pakketprijs: €  150 </br>
+						Massage therapieën en workshops </br>
+						Holistic Pulsing	(ca 1 ½ uur)						€ 60 </br>
+						Gezichtsmassage  met handreflex en hotstones (ca. 50 min)		€ 35 </br>
+						Oorkaarstherapie met oorreflex ( ca 40 min)				€ 30 </br>
+						Workshop Introductie Holistic Pulsing 1 en 2  (dagdeel)			€ 45 </br>
+						Workshop Gezonder door ontzuren	(ca 1 ½ uur)			€ 20 </br>
+						Prijzen zijn inclusief  BTW
+					</h6>
+				</div>
+			</div>
+		</div>
+
+		<?php include($baseComponents.'footer/footer.php'); ?>
+
+		<script type="text/javascript" src="<?php echo $jQuery; ?>"></script>
+		<script type="text/javascript" src="<?php echo $bootstrapJS; ?>"></script>
+	</body>
+</html>
---
+++ Webclient/voorwaarden.php
@@ -0,0 +1,50 @@
+<?php include('globalsettings.php'); ?>
+<!DOCTYPE html>
+<style>
+	.lijstje {
+		margin-top: 20px;
+	}
+	.lijstje>li {
+		padding: 5px 5px;
+	}
+</style>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="<?php echo $bootstrapCSS; ?>" rel="stylesheet" type="text/css">
+		<link href="<?php echo $baseCSS; ?>" rel="stylesheet" type="text/css">
+	</head>
+	<body id='footermargin'>
+		<?php include($baseComponents."header/header.php"); ?>
+
+		<div class="container contentview">
+			<div class="voorwaarden">
+				<div class="col-md-12">
+					<h1 class="text-center" style="margin:0;">Algemene voorwaarden</h1>
+				</div>
+
+				<div class="col-sm-offset-1 col-md-offset-2 col-xs-12 col-sm-10 col-md-8">
+					<ul class="lijstje list-unstyled">
+						<li>Raadpleeg bij gezondheidsklachten altijd eerst een reguliere arts.</li>
+						<li>Massage bij ziekten is mogelijk, maar dan alleen met toestemming van de behandelend arts</li>
+						<li>Medicijngebruik gaarne vooraf melden.</li>
+						<li>Persoonlijke en medische gegevens worden nooit aan derden verstrekt en/of openbaar gemaakt.</li>
+						<li>Contante betaling na consult/behandeling of via overschrijving binnen 7 dagen.</li>
+						<li>Een afspraak afzeggen kan tot 24 uur van tevoren kosteloos. Wanneer u binnen 24 uur afzegt, wordt de besproken tijd in rekening gebracht.</li>
+						<li>Cadeaubonnen zijn niet in te wisselen voor contant geld.</li>
+						<li>Demeter Voeding & Welzijn is niet verantwoordelijk voor beschadiging, verlies of diefstal van persoonlijke bezittingen.</li>
+						<li>Demeter Voeding & Welzijn is niet verantwoordelijk voor letstel aan het lichaam ontstaan tijdens  outdoor trainingen en/of behandelingen.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<?php include($baseComponents.'footer/footer.php'); ?>
+
+		<script type="text/javascript" src="<?php echo $jQuery; ?>"></script>
+		<script type="text/javascript" src="<?php echo $bootstrapJS; ?>"></script>
+	</body>
+</html>
