329fec51d0f7f3a351c4696f4c693c528be83325
Teije van Sloten <teije.van.sloten@hva.nl>
2014-07-04 23:53:32 +0200

    footerchanges

--- Webclient/baseComponents/footer/footer.php
+++ Webclient/baseComponents/footer/footer.php
@@ -1,30 +1,33 @@
 <div id="footerheight" class="footer">
     <div class="container">
-        <div>
-            <div class="alignment">
-                <h4>Algemeen</h4>
-                <ul>
-                    <li><a href="voorwaarden.php">Algemene voorwaarden</a></li>
+            <div class="alignment" style="width: 200px;margin-right: 20px;">
+                <h4 style="border-bottom: 1px solid #CCC;text-align: center;padding-bottom: 8px;">Algemeen</h4>
+                <ul class="list-unstyled" style="margin-left: 30px">
+                    <li><a href="voorwaarden.php">Voorwaarden</a></li>
                     <li><a href="contact.php">Contact</a></li>
                     <li><a href="tarieven.php">Tarieven</a></li>
-                    <li><a href="#">Agenda</a></li>
                 </ul>
             </div>
-            <div class="alignment">
-                <div class="footerimgwrapper">
-                    <img src="<?php echo $img.'demeter.png'; ?>" alt="" class="footerimg">
-                </div>
-                <h4>Demeter, voeding en welzijn</h4>
-                <ul class="list-unstyled">
+            <div class="alignment" style="width: 200px;margin-left: 20px;margin-right: 45px;">
+                <h4 style="border-bottom: 1px solid #CCC;text-align: center;padding-bottom: 8px;">Contact</h4>
+                <ul class="list-unstyled" style="margin-left:20px;">
                     <li>Langelende laan 4</li>
                     <li><a href="mailto:info@demetervw.nl">info@demetervw.nl</a></li>
                     <li>tel. 0612345678</li>
                 </ul>
             </div>
-        </div>
+            <div class="alignment" style="width: 200px;">
+                <div class="footerimgwrapper">
+                    <img src="<?php echo $img . 'demeter.png'; ?>" alt="" class="footerimg">
+                </div>
+                <div class="col-xs-6"><img src="" /></div>
+                <div class="col-xs-6"><img src="" /></div>
+                
+                    
+            </div>
         <div class="col-md-12">
             <p class="footerpower">Powered by <a href="">Webcms</a></p>
         </div>
     </div>
 </div>
-<script type="text/javascript" src="<?php echo $js.'footermargin.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $js . 'footermargin.js'; ?>"></script>
