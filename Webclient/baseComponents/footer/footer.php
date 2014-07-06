4c59f878e52203a3037b262cff1083bea459a565
Teije van Sloten <teije.van.sloten@hva.nl>
2014-07-06 12:42:01 +0200

    Footer improvements. Email button needs some improvements.

--- Webclient/baseComponents/footer/footer.php
+++ Webclient/baseComponents/footer/footer.php
@@ -1,29 +1,33 @@
 <div id="footerheight" class="footer">
     <div class="container">
-            <div class="alignment" style="width: 200px;margin-right: 20px;">
-                <h4 style="border-bottom: 1px solid #CCC;text-align: center;padding-bottom: 8px;">Algemeen</h4>
-                <ul class="list-unstyled" style="margin-left: 30px">
+            <div class="alignment">
+                <h4>Algemeen</h4>
+                <ul class="list-unstyled">
                     <li><a href="voorwaarden.php">Voorwaarden</a></li>
                     <li><a href="contact.php">Contact</a></li>
                     <li><a href="tarieven.php">Tarieven</a></li>
                 </ul>
             </div>
-            <div class="alignment" style="width: 200px;margin-left: 20px;margin-right: 45px;">
-                <h4 style="border-bottom: 1px solid #CCC;text-align: center;padding-bottom: 8px;">Contact</h4>
-                <ul class="list-unstyled" style="margin-left:20px;">
+            <div class="alignment">
+                <h4>Contact</h4>
+                <ul class="list-unstyled">
                     <li>Langelende laan 4</li>
                     <li><a href="mailto:info@demetervw.nl">info@demetervw.nl</a></li>
                     <li>tel. 0612345678</li>
                 </ul>
             </div>
-            <div class="alignment" style="width: 200px;">
-                <div class="footerimgwrapper">
+            <div class="alignment">
+                <div class="footerwrapper">
+                    <div style="display: inline-block">
+                        <img src="<?php echo $img. 'fbicon.png';?>" class="mediabutton" />
+                    </div>
+                    <div style="display: inline-block">
+                        <img src="<?php echo $img. 'email.png';?>" class="mediabutton" />
+                    </div>
+                </div>
+                <div class="footerwrapper">
                     <img src="<?php echo $img . 'demeter.png'; ?>" alt="" class="footerimg">
                 </div>
-                <div class="col-xs-6"><img src="" /></div>
-                <div class="col-xs-6"><img src="" /></div>
-                
-                    
             </div>
         <div class="col-md-12">
             <p class="footerpower">Powered by <a href="">Webcms</a></p>
