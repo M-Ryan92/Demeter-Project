beb0046ae70a120a8cbbd0a28aa3be239002adf2
Teije van Sloten <teije.van.sloten@hva.nl>
2014-07-16 16:12:47 +0200

    Minor fixes.

--- Webclient/baseComponents/footer/footer.php
+++ Webclient/baseComponents/footer/footer.php
@@ -1,40 +1,42 @@
 <div id="footerheight" class="footer">
     <div class="footerwrapper">
-            <div class="alignment">
-                <h4>Algemeen</h4>
-                <ul class="list-unstyled">
-                    <li><a href="voorwaarden.php">Voorwaarden</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="tarieven.php">Tarieven</a></li>
-                </ul>
-            </div>
-            <div class="alignment">
-                <h4>Contact</h4>
-                <ul class="list-unstyled">
-                    <li>Langelende laan 4</li>
-                    <li><a href="mailto:info@demetervw.nl">info@demetervw.nl</a></li>
-                    <li>tel. 0612345678</li>
-                </ul>
-            </div>
-            <div class="alignment">
-                <div class="footerwrapper">
-                    <div style="display: inline-block">
-                        <a href="https://nl-nl.facebook.com/pages/Demeter-Voeding-Welzijn/564168070261116">
-                            <img src="<?php echo $img. 'fbicon.png';?>" class="mediabutton" />
-                        </a>
-                    </div>
-                    <div style="display: inline-block">
-                        <a href="mailto:info@demetervw.nl">
-                            <img src="<?php echo $img. 'email0.png';?>" class="mediabutton" />
-                        </a>
-                    </div>
+        <div class="alignment">
+            <h4>Algemeen</h4>
+            <ul class="list-unstyled">
+                <li><a href="voorwaarden.php">Voorwaarden</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="tarieven.php">Tarieven</a></li>
+            </ul>
+        </div>
+        <div class="alignment">
+            <h4>Contact</h4>
+            <ul class="list-unstyled">
+                <li>Langelende laan 4</li>
+                <li><a href="mailto:info@demetervw.nl">info@demetervw.nl</a></li>
+                <li>tel. 0612345678</li>
+            </ul>
+        </div>
+        <div class="alignment">
+            <div class="footerwrapper">
+                <div style="display: inline-block">
+                    <a href="https://nl-nl.facebook.com/pages/Demeter-Voeding-Welzijn/564168070261116">
+                        <img src="<?php echo $img . 'fbicon.png'; ?>" class="mediabutton" />
+                    </a>
                 </div>
-                <div class="footerwrapper">
+                <div style="display: inline-block">
+                    <a href="mailto:info@demetervw.nl">
+                        <img src="<?php echo $img . 'email0.png'; ?>" class="mediabutton" />
+                    </a>
+                </div>
+            </div>
+            <div class="footerwrapper">
+                <a href="basicskin.php">
                     <img src="<?php echo $img . 'demeter.png'; ?>" alt="" class="footerimg">
-                </div>
+                </a>
             </div>
+        </div>
         <div class="col-md-12">
-            <p class="footerpower">Powered by <a href="">Webcms</a></p>
+            <p class="footerpower">Powered by <a href="#">Webcms</a></p>
         </div>
     </div>
 </div>
--- Webclient/baseComponents/header/header.php
+++ Webclient/baseComponents/header/header.php
@@ -9,8 +9,8 @@
                     </span>
                 </div>
             </div>
-            <img class="navbar-brand" src="<?php echo $img.'demeter.png'; ?>" alt="">
-            <span class="header-title">Demeter voeding en welzijn</span>
+            <img class="navbar-brand" src="<?php echo $img . 'demeter.png'; ?>" alt="">
+            <span class="header-title">Demeter Voeding en Welzijn</span>
             <button type="button" class="navbar-toggle navbar-right"  data-toggle="collapse" data-target=".dropdownmenu">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
@@ -31,16 +31,16 @@
         </div>
     </div>
     <!-- <div class="header-label"> -->
-        <div class="dropdownmenu collapse">
-            <ul class="nav">
-                <li><a href="basicskin.php">Home</a></li>
-                <li><a href="Category.php">Workshops overzicht</a></li>
-                <li><a href="workshop.php">Workshop details</a></li>
-                <li><a href="tarieven.php">tarieven</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#cat3">Cat3</a></li>
-            </ul>
-        </div>
+    <div class="dropdownmenu collapse">
+        <ul class="nav">
+            <li><a href="basicskin.php">Home</a></li>
+            <li><a href="Category.php">Workshops overzicht</a></li>
+            <li><a href="workshop.php">Workshop details</a></li>
+            <li><a href="tarieven.php">tarieven</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#cat3">Cat3</a></li>
+        </ul>
+    </div>
     <!-- </div> -->
 </div>
