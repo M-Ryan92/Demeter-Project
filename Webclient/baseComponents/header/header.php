da0ced446ba28909e9986d1b4deaf197cfe30198
Teije van Sloten <teije.van.sloten@hva.nl>
2014-07-20 16:36:49 +0200

    New header, full size.

--- Webclient/baseComponents/header/header.php
+++ Webclient/baseComponents/header/header.php
@@ -9,7 +9,7 @@
                     </span>
                 </div>
             </div>
-            <img class="navbar-brand" src="<?php echo $img.'demeter.png'; ?>" alt="">
+            <img class="navbar-brand" src="<?php echo $img . 'demeter.png'; ?>" alt="">
             <span class="header-title">Demeter Voeding en Welzijn</span>
             <button type="button" class="navbar-toggle navbar-right"  data-toggle="collapse" data-target=".dropdownmenu">
                 <span class="sr-only">Toggle navigation</span>
@@ -20,23 +20,45 @@
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li><a href="basicskin.php">Home</a></li>
-                    <li><a href="Category.php">Workshops overzicht</a></li>
-                    <li><a href="tarieven.php">tarieven</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Voeding <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Sportvoeding</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Natuurvoeding</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Therapieën <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Holistic Pulising</a></li>
+                            <li class="divider"></li>
+                            <li><a href="/Gezichtsmassage.php">Gezichtsmassage</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Oorkaars-therapie</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Ontzuren</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Activiteiten <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Outdoor-training</a></li>
+                        </ul>
+                    </li>
                     <li><a href="contact.php">Contact</a></li>
-                    <li><a href="#about">About</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <!-- <div class="header-label"> -->
-        <div class="dropdownmenu collapse">
-            <ul class="nav">
-                <li><a href="basicskin.php">Home</a></li>
-                <li><a href="Category.php">Workshops overzicht</a></li>
-                <li><a href="tarieven.php">tarieven</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="#about">About</a></li>
-            </ul>
-        </div>
+    <div class="dropdownmenu collapse">
+        <ul class="nav">
+            <li><a href="basicskin.php">Home</a></li>
+            <li><a href="Category.php">Workshops overzicht</a></li>
+            <li><a href="tarieven.php">tarieven</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
     <!-- </div> -->
 </div>
